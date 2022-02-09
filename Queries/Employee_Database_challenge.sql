c3df475c2bb527783f9989fa2cda566705a16ed0
matt Rickle <matt.rickle@outlook.com>
2022-02-09 13:21:13 -0500

    adding challenge materials

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -19,3 +19,12 @@ from unique_titles
 group by title
 order by count(title) desc;
 
+select distinct on (emp_no) e.emp_no, e.first_name, e.last_name, e.birth_date, de.from_date, de.to_date, t.title
+into mentorship_eligibilty
+from employees as e
+join dept_emp as de
+on e.emp_no = de.emp_no
+join titles as t
+on e.emp_no = t.emp_no
+where de.to_date = '9999-01-01' and (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+order by emp_no
